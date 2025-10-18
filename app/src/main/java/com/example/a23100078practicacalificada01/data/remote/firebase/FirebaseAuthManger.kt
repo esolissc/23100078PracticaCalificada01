f7b82c64a67b076e1553f54dfb9cad2468cbc722
Edgar <esolis.sc@gmail.com>
2025-10-18 16:41:20 -0500

    Initial commit

---
+++ app/src/main/java/com/example/a23100078practicacalificada01/data/remote/firebase/FirebaseAuthManger.kt
@@ -0,0 +1,46 @@
+package com.example.a23100078practicacalificada01.data.remote.firebase
+
+import com.google.firebase.Timestamp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
+object FirebaseAuthManger {
+    private val auth = FirebaseAuth.getInstance()
+    private val firestore = FirebaseFirestore.getInstance()
+
+    //Register User
+    suspend fun registerUser(email: String,
+                             name: String,
+                             lastName: String,
+                             phone: String,
+                             password: String):
+    Result<Unit>{
+        return try {
+            val authResult = auth.createUserWithEmailAndPassword(email, password).await()
+            val uid = authResult.user?.uid.toString()
+
+            val user = hashMapOf(
+                "uid" to uid,
+                "email" to email,
+                "name" to name,
+                "lastName" to lastName,
+                "phone" to phone,
+            )
+            firestore.collection("users").document(uid).set(user).await()
+            Result.success(Unit)
+        } catch (e: Exception){
+            Result.failure(e)
+        }
+    }
+
+    //Login User
+    suspend fun loginUser(email: String, password: String): Result<Unit>{
+        return try {
+            auth.signInWithEmailAndPassword(email, password).await()
+            Result.success(Unit)
+        } catch (e: Exception){
+            Result.failure(e)
+        }
+    }
+}
